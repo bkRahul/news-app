8e3a345477937252a8e8095da7698d69e590e033
bkRahul <rahulkrishnappa94@gmail.com>
2021-04-01 00:26:19 +0530

    added grid and layout hoc

---
+++ src/hoc/GridLayout/withGridLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styled from "styled-components";
+import { LaptopWidth } from "../../components/CommonStyles";
+
+const GridWrapper = styled(LaptopWidth)`
+  display: grid;
+  grid-gap: 1.5em;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, auto);
+`;
+
+export const withGridLayout = ({ children }) => {
+  return <GridWrapper>{children}</GridWrapper>;
+};
