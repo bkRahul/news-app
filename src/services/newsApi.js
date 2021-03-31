cfea5b2273db43200f258822328c15a205d24f8b
bkRahul <rahulkrishnappa94@gmail.com>
2021-04-01 00:27:22 +0530

    added some of services

---
+++ src/services/newsApi.js
@@ -0,0 +1,47 @@
+import axios from "../axios-news";
+
+const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+
+export const getLocation = async () => {
+  const result = await axios
+    .get("https://ipapi.co/json/")
+    .then(({ data }) => data.country)
+    .catch((err) => console.log(err));
+  return result;
+};
+
+export const getLocalHeadlines = async (country) => {
+  const result = await axios
+    .get(`top-headlines/?country=${country}&apiKey=${apiKey}`)
+    .then(({ data }) => data)
+    .catch((err) => console.log(err));
+  return result;
+};
+
+export const getHeadlines = async (country, q, pageSize, page) => {
+  const result = await axios
+    .get(
+      `top-headlines/?country=${country}&q=${q}&pageSize=${pageSize}&page=${page}&apiKey=${apiKey}`
+    )
+    .then(({ data }) => data)
+    .catch((err) => console.log(err));
+  return result;
+};
+
+export const getEverything = async (q, pageSize, page) => {
+  const result = await axios
+    .get(
+      `everything/?q=${q}&pageSize=${pageSize}&page=${page}&apiKey=${apiKey}`
+    )
+    .then(({ data }) => data)
+    .catch((err) => console.log(err));
+  return result;
+};
+
+export const getSources = async (q, language) => {
+  const result = await axios
+    .get(`sources/?q=${q}&language=${language}&apiKey=${apiKey}`)
+    .then(({ data }) => data)
+    .catch((err) => console.log(err));
+  return result;
+};
