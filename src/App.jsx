5914ac02936b7513cd9555000f3e0368240d6a78
bkRahul <rahulkrishnappa94@gmail.com>
2021-04-01 00:25:12 +0530

    added router and hoc changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Switch } from "react-router-dom";
+
+import { Home } from "./containers/HomePage";
+import { withLayout as WithLayout } from "./hoc/Layout/withLayout";
+
+export const App = () => {
+  return (
+    <WithLayout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </WithLayout>
+  );
+};
