8f2fd30bc769e93029ea006f6bb85d0e3d783bc4
bkRahul <rahulkrishnappa94@gmail.com>
2021-04-01 00:27:57 +0530

    added category title component

---
+++ src/components/SharedComponents/CategoryTitle/CategoryTitle.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+const TitleWrapper = styled.div`
+  text-align: center;
+  width: 100%;
+  margin-bottom: 3em;
+`;
+
+const Title = styled.div`
+  color: #33353b;
+  font-size: 2.8em;
+  font-weight: 600;
+  position: relative;
+  margin-bottom: 23px;
+
+  &:after {
+    content: "";
+    position: absolute;
+    width: 70px;
+    height: 2px;
+    background: #ff7a7f;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: -18px;
+  }
+`;
+
+const Description = styled.p`
+  color: #0a0a0a;
+  line-height: 3;
+  font-size: 0.9em;
+`;
+
+export const CategoryTitle = ({ title, description }) => {
+  return (
+    <TitleWrapper>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </TitleWrapper>
+  );
+};
